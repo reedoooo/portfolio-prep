cf3b5e20bf1dd9d8c7c470c929eda13a20fa032d
reedoooo <readvogt@gmail.com>
2023-03-10 03:11:58 -0700

    Fixing the JSON Now

--- src/components/MyStuff/Practice.js
+++ src/components/MyStuff/Practice.js
@@ -15,3 +15,73 @@ export default class Practice extends Component {
     )
   }
 }
+
+// class App extends Component {
+//   titles = [];
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       foo: "bar",
+//       profileData: {},
+//       projectData: {},
+//     };
+//   }
+
+//   componentDidMount = () => {
+//     this.loadProjectData("projects.json");
+//     this.loadProfileData("profile.json");
+//   };
+//   loadProfileData = () => {
+//     $.ajax({
+//       url: `https://raw.githubusercontent.com/reedoooo/portfolio-prep/ec448c2d8401ec6dd2892d6ba5e7fca5a158d374/public/profile.json`,
+//       dataType: "json",
+//       cache: false,
+//       success: function (data) {
+//         this.setState({ profileData: data });
+//       }.bind(this),
+//       error: function (xhr, status, err) {
+//         console.log("Error loading profile data: ", err);
+//         alert(err);
+//       },
+//     });
+//   };
+
+//   loadProjectData = () => {
+//     $.ajax({
+//       url: `https://raw.githubusercontent.com/reedoooo/portfolio-prep/ec448c2d8401ec6dd2892d6ba5e7fca5a158d374/public/projects.json`,
+//       dataType: "json",
+//       cache: false,
+//       success: function (data) {
+//         this.setState(
+//           { projectData: data },
+//           () => (document.title = `${this.state.projectData.basic_info.name}`)
+//         );
+//       }.bind(this),
+//       error: function (xhr, status, err) {
+//         console.log("Error loading project data: ", err);
+//         alert(err);
+//       },
+//     });
+//   };
+
+//   render() {
+//     console.log(this.state.projectData);
+//     console.log(this.state.profileData);
+
+//     return (
+//       <div className="MASTER-DIV">
+//         <Header
+//           profileData={this.state.profileData.basic_info}
+//           projectData={this.state.projectData.basic_info}
+//         />
+//         <Main
+//           profileData={this.state.profileData}
+//           projectData={this.state.projectData}
+//         />
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
